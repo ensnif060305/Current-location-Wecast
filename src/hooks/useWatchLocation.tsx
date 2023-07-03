3f4c15a2f976a00fd7e6066ab85d1c8a7518d7f1
ensnif060305 <jmsun0305@gmail.com>
2023-07-03 17:29:34 +0900

    feat: Hook using Geolocation API

---
+++ src/hooks/useWatchLocation.tsx
@@ -0,0 +1,53 @@
+import { useRef, useState, useEffect } from "react"
+
+interface ILocation {
+  latitude: number
+  longitude: number
+}
+
+const useWatchLocation = (option = {}) => {
+  const [location, setLocation] = useState<ILocation>()
+  const [error, setError] = useState<string>("")
+  const locationWatchId = useRef<number | undefined>()
+
+  const handleSuccess = (pos: GeolocationPosition) => {
+    const { latitude, longitude } = pos.coords
+    setLocation({
+      latitude,
+      longitude,
+    })
+  }
+
+  const handleError = (err: GeolocationPositionError) => {
+    setError(err.message)
+  }
+
+  const cancelWatchLocation = () => {
+    const { geolocation } = navigator
+
+    if (locationWatchId.current && geolocation) {
+      geolocation.clearWatch(locationWatchId.current)
+    }
+  }
+
+  useEffect(() => {
+    const { geolocation } = navigator
+
+    if (!geolocation) {
+      setError("Geolocation is not working")
+      return
+    }
+
+    locationWatchId.current = geolocation.watchPosition(
+      handleSuccess,
+      handleError,
+      option,
+    )
+
+    return cancelWatchLocation
+  }, [option])
+
+  return { location, cancelWatchLocation, error }
+}
+
+export default useWatchLocation
