ed772e3837fc9a26ad3dd7a49ef702c2c3f34b4a
ensnif060305 <jmsun0305@gmail.com>
2023-07-20 10:18:34 +0900

    feat: Add function to convert date to day of week

---
+++ src/utils/dayOfWeek.tsx
@@ -0,0 +1,21 @@
+export const getDayofWeek = (day: number) => {
+  const dayOfWeek = new Date(day).getDay()
+  switch (dayOfWeek) {
+    case 0:
+      return "Sun"
+    case 1:
+      return "Mon"
+    case 2:
+      return "Tue"
+    case 3:
+      return "Wed"
+    case 4:
+      return "Thu"
+    case 5:
+      return "Fri"
+    case 6:
+      return "Sat"
+    default:
+      return ""
+  }
+}
