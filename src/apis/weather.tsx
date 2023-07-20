727c6aa691d6512834c7e9e704edf1a1e83d6070
ensnif060305 <jmsun0305@gmail.com>
2023-07-20 10:20:26 +0900

    docx: Change base url to env settings file

--- src/apis/weather.tsx
+++ src/apis/weather.tsx
@@ -1,12 +1,14 @@
 import axios from "axios"
 import { useQuery, useQueryClient } from "@tanstack/react-query"
 import { Response } from "./type"
+
+const BaseURL =  process.env.NEXT_PUBLIC_WEATHER_API_BASE_URL
 const ServiceKey = process.env.NEXT_PUBLIC_WEATHER_API_SERVICE_KEY
 
 export const useGetWeather = (boo: boolean, data: { x: number; y: number }) => {
   const queryClient = useQueryClient()
   const fetch = async (): Promise<Response> =>
-    await axios.get("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst", {
+    await axios.get(`${BaseURL}/getVilageFcst`, {
       params: {
         serviceKey: ServiceKey,
         pageNo: "1",
